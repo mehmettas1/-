24e56d6cc42d19a5fc2353507bc7c4fe6064a398
mehmet <tas349873@gmail.com>
2022-07-12 00:38:55 +0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
+import {NavbarContainer} from './Style'
+import {NavLink} from 'react-router-dom'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <NavbarContainer>
+      <div className="navbar-left">
+        <NavLink to= "/home"> <i>{"<CLARUSWAY>"}</i> <span>RECIPE</span></NavLink>
+      </div>
+      <NavLink to='./about'>ABOUT</NavLink>
+      <a href="_blank" rel='noreferrer' href ="https://github.com/mehmettas1">GITHUB</a>
+      <NavLink to='./'>LOGOUT</NavLink>
+    </NavbarContainer>
   )
 }
 
